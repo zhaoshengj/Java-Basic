a4ae140fc54ca0afbe3329445ed77671ab2fe026
zhaoshengjin <zhaoshengjin@shopuu.com>
2020-04-04 20:55:33 +0800

    init

---
+++ src/test/java/com/zsj/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsj.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
